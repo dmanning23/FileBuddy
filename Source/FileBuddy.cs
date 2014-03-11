43776f9099872be5bc7ec886012e0b562de046be
unknown <dmanning23@hotmail.com>
2014-03-10 23:01:23 -0500

    finished up here

--- Source/FileBuddy.cs
+++ Source/FileBuddy.cs
@@ -1,15 +1,14 @@
 ﻿using EasyStorage;
+using FilenameBuddy;
 using Microsoft.Xna.Framework;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Xml;
 
 namespace FileBuddyLib
 {
 	/// <summary>
-	/// This s
+	/// This class does all the heavy lifting for saving/loading files.
 	/// </summary>
 	public abstract class FileBuddy
 	{
@@ -21,24 +20,27 @@ namespace FileBuddyLib
 		private IAsyncSaveDevice saveDevice;
 		
 		/// <summary>
-		/// The location to store this high score list.
-		/// This will be a relative path from the user directory, so don't put in drive letters etc.
+		/// The location where this file will be saved/loaded from.
 		/// </summary>
-		/// <value>The name of the folder.</value>
-		private string Folder { get; set; }
-
-		/// <summary>
-		/// The location to store this high score list.
-		/// This will be a relative path from the user directory, so don't put in drive letters etc.
-		/// </summary>
-		/// <value>The name of the folder.</value>
-		public string F { get; set; }
+		public Filename File { get; set; }
 
 		/// <summary>
 		/// Flag for whether or not teh high scores have been loaded from a file
 		/// </summary>
 		/// <value><c>true</c> if loaded; otherwise, <c>false</c>.</value>
 		private bool Loaded { get; set; }
+
+		/// <summary>
+		/// delegate method to write out to file, provided by user.
+		/// </summary>
+		/// <param name="myFileStream"></param>
+		private FileAction SaveMethod;
+
+		/// <summary>
+		/// delegate method to load from file, provided by user.
+		/// </summary>
+		/// <param name="myFileStream"></param>
+		private FileAction LoadMethod;
 
 		#endregion //Member Variables
 
@@ -47,30 +49,24 @@ namespace FileBuddyLib
 		/// <summary>
 		/// hello standard constructor!
 		/// </summary>
-		public HighScoreTable(string FolderLocation)
+		public FileBuddy(Filename fileLocation, FileAction save, FileAction load)
 		{
 			//set the save location
-			Folder = FolderLocation;
+			File = fileLocation;
 
-			HighScoreLists = new Dictionary<string, HighScoreList>();
+			SaveMethod = save;
+			LoadMethod = load;
+
 			Loaded = false;
 		}
-
-		/// <summary>
-		/// Adds all the lists to the table.
-		/// </summary>
-		protected abstract void AddLists();
 
 		/// <summary>
 		/// called once at the beginning of the program
 		/// gets the storage device
 		/// </summary>
 		/// <param name="myGame">the current game.</param>
-		public void Initialize(Game myGame)
+		public virtual void Initialize(Game myGame)
 		{
-			//First add the default lists to the table
-			AddLists();
-
 			// on Windows Phone we use a save device that uses IsolatedStorage
 			// on Windows and Xbox 360, we use a save device that gets a shared StorageDevice to handle our file IO.
 #if WINDOWS_PHONE || ANDROID
@@ -118,24 +114,6 @@ namespace FileBuddyLib
 			Debug.WriteLine(strText);
 		}
 
-		/// <summary>
-		/// Gets the high score list by name
-		/// </summary>
-		/// <returns>The high score list.</returns>
-		/// <param name="ListName">name of teh high score list to fetch.</param>
-		public HighScoreList GetHighScoreList(string ListName)
-		{
-			if (HighScoreLists.ContainsKey(ListName))
-			{
-				return HighScoreLists[ListName];
-			}
-			else
-			{
-				//list does not exist
-				return null;
-			}
-		}
-
 		#endregion //Methods
 
 		#region XML Methods
@@ -148,12 +126,12 @@ namespace FileBuddyLib
 			// make sure the device is ready
 			if (saveDevice.IsReady)
 			{
-				// save a file asynchronously. this will trigger IsBusy to return true
-				// for the duration of the save process.
+				//save a file asynchronously. 
+				//this will trigger IsBusy to return true for the duration of the save process.
 				saveDevice.SaveAsync(
-					Folder,
-					"HighScores.xml",
-					WriteHighScores);
+					File.GetPath(),
+					File.GetFile(),
+					SaveMethod);
 			}
 		}
 
@@ -164,17 +142,17 @@ namespace FileBuddyLib
 				try
 				{
 					//if there is a file there, load it into the system
-					if (saveDevice.FileExists(Folder, "HighScores.xml"))
+					if (saveDevice.FileExists(File.GetPath(), File.GetFile()))
 					{
 						saveDevice.Load(
-							Folder,
-							"HighScores.xml",
-							ReadHighScores);
+							File.GetPath(),
+							File.GetFile(),
+							LoadMethod);
 					}
 
 					//set the Loaded flag to true since high scores only need to be laoded once
 					Loaded = true;
-					Debug.WriteLine("Loaded High Scores");
+					Debug.WriteLine("Loaded file" + File.File);
 				}
 				catch (Exception ex)
 				{
@@ -184,110 +162,6 @@ namespace FileBuddyLib
 					// just write some debug output for our verification
 					Debug.WriteLine(ex.Message);
 				}
-			}
-		}
-
-		/// <summary>
-		/// do the actual writing out to disk
-		/// </summary>
-		/// <param name="myFileStream">My file stream.</param>
-		private void WriteHighScores(Stream myFileStream)
-		{
-			try
-			{
-				//open the file, create it if it doesnt exist yet
-				XmlTextWriter rFile = new XmlTextWriter(myFileStream, null);
-				rFile.Formatting = Formatting.Indented;
-				rFile.Indentation = 1;
-				rFile.IndentChar = '\t';
-				
-				//save all the high scores!
-				rFile.WriteStartDocument();
-
-				//write the high score table element
-				rFile.WriteStartElement("highscoretable");
-
-				//write out all the lists
-				foreach(KeyValuePair<string, HighScoreList> entry in HighScoreLists)
-				{
-					// do something with entry.Value or entry.Key
-					entry.Value.WriteToXML(rFile);
-				}
-
-				rFile.WriteEndElement();
-
-				rFile.WriteEndDocument();
-				
-				// Close the file.
-				rFile.Flush();
-				rFile.Close();
-			}
-			catch (Exception ex)
-			{
-				// just write some debug output for our verification
-				Debug.WriteLine(ex.Message);
-			}
-		}
-
-		/// <summary>
-		/// to the actual reading in from disk
-		/// </summary>
-		/// <param name="myFileStream">My file stream.</param>
-		private void ReadHighScores(Stream myFileStream)
-		{
-			XmlDocument xmlDoc = new XmlDocument();
-			xmlDoc.Load(myFileStream);
-			XmlNode rootNode = xmlDoc.DocumentElement;
-
-			//make sure it is actually an xml node
-			if (rootNode.NodeType == XmlNodeType.Element)
-			{
-				//make sure is correct type of node
-				Debug.Assert("highscoretable" == rootNode.Name);
-
-				//read high score lists
-				if (rootNode.HasChildNodes)
-				{
-					XmlNode childNode = rootNode.FirstChild;
-					while (childNode != null)
-					{
-						//get the name of this list
-						string ListName = "";
-						XmlNamedNodeMap mapAttributes = childNode.Attributes;
-						for (int i = 0; i < mapAttributes.Count; i++)
-						{
-							//will only have the name attribute
-							string strName = mapAttributes.Item(i).Name;
-							string strValue = mapAttributes.Item(i).Value;
-							if ("ListName" == strName)
-							{
-								ListName = strValue;
-							}
-							else
-							{
-								//unknwon attribute in the xml file!!!
-								Debug.Assert(false);
-							}
-						}
-
-						//find the list from the xml file
-						Debug.Assert(!String.IsNullOrEmpty(ListName));
-						HighScoreList rHighScoreList = HighScoreLists[ListName];
-						Debug.Assert(null != rHighScoreList);
-
-						//create, read, and store a new high score list from this xml node
-						rHighScoreList.ReadFromXML(childNode);
-						HighScoreLists[rHighScoreList.Name] = rHighScoreList;
-						
-						//next node!
-						childNode = childNode.NextSibling;
-					}
-				}
-			}
-			else
-			{
-				//should be an xml node!!!
-				Debug.Assert(false);
 			}
 		}
 
